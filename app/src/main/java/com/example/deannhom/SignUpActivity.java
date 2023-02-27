a552a0c173f34c784350953218063d6818f4c242
MinhQuan9812 <minwonasd123@gmail.com>
2023-02-27 07:11:06 +0700

    Login Screen

---
+++ app/src/main/java/com/example/deannhom/SignUpActivity.java
@@ -0,0 +1,27 @@
+package com.example.deannhom;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        ImageButton mBackButton= (ImageButton) findViewById(R.id.backButton);
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignUpActivity.this,MainActivity.class));
+            }
+        });
+    }
+
+
+}
