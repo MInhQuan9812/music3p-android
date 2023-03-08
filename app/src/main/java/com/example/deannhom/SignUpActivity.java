a3021996f0362055475eea00674ef7b70c25ef5c
Shinji Makoto <minhlong882003@gmail.com>
2023-03-08 21:19:41 +0700

    Update tính năng đăng ký, đăng nhập, xem description
    Lưu ý: Trên máy tui nó đã login nên sẽ bỏ qua file login mà vào file home, không biết mọi người như nào nên là cứ chạy lại app thử
    
    Open an issue: Nếu có bất kỳ bug gì cần tui sửa nhé

--- app/src/main/java/com/example/deannhom/SignUpActivity.java
+++ app/src/main/java/com/example/deannhom/SignUpActivity.java
@@ -1,23 +1,64 @@
 package com.example.deannhom;
-
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText mFullNameEditText;
+    private EditText mEmailEditText;
+    private EditText mPasswordEditText;
+    private EditText mConfirmPasswordEditText;
+    private Button mSignUpButton;
+    private SharedPreferences mSharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        ImageButton mBackButton= (ImageButton) findViewById(R.id.backButton);
-        mBackButton.setOnClickListener(new View.OnClickListener() {
+        // Initialize SharedPreferences
+        mSharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+
+        // Initialize views
+        mFullNameEditText = findViewById(R.id.fullNameEditText);
+        mEmailEditText = findViewById(R.id.emailEditText);
+        mPasswordEditText = findViewById(R.id.passwordEditText);
+        mConfirmPasswordEditText = findViewById(R.id.confirmPasswordEditText);
+        mSignUpButton = findViewById(R.id.signUpButton);
+
+        // Set onClickListener for signUpButton
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(SignUpActivity.this, WellcomeActivity.class));
+                // Get user input
+                String fullName = mFullNameEditText.getText().toString();
+                String email = mEmailEditText.getText().toString();
+                String password = mPasswordEditText.getText().toString();
+                String confirmPassword = mConfirmPasswordEditText.getText().toString();
+
+                // Check if input is valid
+                if (fullName.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+                    Toast.makeText(SignUpActivity.this, "Please enter all required information", Toast.LENGTH_SHORT).show();
+                } else if (!password.equals(confirmPassword)) {
+                    Toast.makeText(SignUpActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                } else {
+                    // If input is valid, save sign-up information and go to main activity
+                    SharedPreferences.Editor editor = mSharedPreferences.edit();
+                    editor.putString("fullName", fullName);
+                    editor.putString("email", email);
+                    editor.putString("password", password);
+                    editor.apply();
+
+                    startActivity(new Intent(SignUpActivity.this, HomeActivity.class));
+                    finish();
+                }
             }
         });
     }
